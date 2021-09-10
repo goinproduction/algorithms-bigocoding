1482be9dbc73bb205d2463b91c9a8dfb20c553d4
viincyy <viincyy126@gmail.com>
2021-09-11 02:30:37 +0700

    Theory

---
+++ PRACTICE/twopointers.cpp
@@ -0,0 +1,34 @@
+// int ans = 0, sum = 0;
+// for (int l = 1, r = 1; r <= n; r++) {
+//     sum += a[r];
+//     while (sum > s) {
+//       sum -= a[l];
+//       l++;
+//     }
+//     ans = max(ans, r - l + 1);
+// }
+// cout << ans;
+
+// int i = 1, j = N;
+// while (i < j) {
+//     if (a[i] + a[j] == x) {
+//         cout << i << " " << j;
+//         return 0;
+//     }
+//     if (a[i] + a[j] < x)
+//         i += 1;
+//     else
+//         j -= 1;
+// }
+// cout << "No solution";
+
+// int i = 1, j = 1;
+// vector<int> c;
+// while (i <= n || j <= m){
+//     if (j == m + 1 || (i <= n && a[i] <= b[j]))
+//         c.push_back(a[i++]);
+//     else
+//         c.push_back(b[j++]);
+// }
+// for (auto it: c)
+//     cout << it << " ";
